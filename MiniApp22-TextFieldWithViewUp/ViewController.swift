963e8b313f8b489d1d9cf6c3e8168f6242709e22
MaedaKouta <maedakoutaa@gmail.com>
2022-03-05 11:47:16 +0900

    commit

--- MiniApp22-TextFieldWithViewUp/ViewController.swift
+++ MiniApp22-TextFieldWithViewUp/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var textField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+
+        let noneMoveHeight = view.frame.origin.y + view.frame.size.height - keyboardSize.height
+
+        if noneMoveHeight <= textField.frame.midY {
+            if view.frame.origin.y == 0 {
+                view.frame.origin.y -= keyboardSize.height
+            } else {
+                let gapHeight = view.frame.origin.y + keyboardSize.height
+                view.frame.origin.y -= gapHeight
+            }
+        } else {
+            print("キーボードを動かす必要なし")
+        }
 }
 
+    @objc func keyboardWillHide() {
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
+    }
+
+}
